7c5d70c6768698c1329cc98b2320359db9ece742
fraaannn <wellplay666@outlook.com>
2018-05-04 23:46:32 +0800

    'test_flask'

---
+++ flask_test.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return '<h2>welcome to cookie pool system</h2>'
+
+
+@app.route('/<website>/random')
+def random(website):
+    data = '<h2>%s</h2>'%str(website)
+
+
+if __name__ == "__main__":
+    app.run()
